564e25474b2150c542f3796a8d6db234da7c1559
worldofchris <chris@worldofchris.com>
2020-02-14 11:26:08 +0000

    bigger, better

--- spike.js
+++ spike.js
@@ -2,7 +2,9 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const Bezier = require('bezier-js');
 const lcg = require('./lcg.js');
 
-const mapSize = 64;
+const mapSize = 255;
+const numLutPoints = mapSize * 2;
+const numLines = 9;
 
 const doc = new GoogleSpreadsheet('1evafmirr85-FMAYFhmlsS-jZ62vwrPgo3cISmSjXSrI');
 doc.useServiceAccountAuth(require('./bc-map-268007-7e2ca0c52eb6.json'))
@@ -18,23 +20,14 @@ doc.useServiceAccountAuth(require('./bc-map-268007-7e2ca0c52eb6.json'))
                                       {pixelSize: 5},
                                       {startIndex: 0, endIndex: mapSize})
 
-      sheet.loadCells('A1:HG120').then(function(value) {
+      sheet.loadCells({startRowIndex: 0, 
+                      endRowIndex: mapSize,
+                      startColumnIndex:0,
+                      endColumnIndex: mapSize}).then(function(value) {
       var i = 0;
       var seq = lcg(mapSize*i,1, mapSize, 6)
-      var colours = [
-        { red: 0.91764706, green: 0.2627451, blue: 0.20784314 },
-        { red: 0.003921569, green: 0.9647059, blue: 0.9647059 },
-        { red: 0.99607843, green: 0.6117647, blue: 0.9647059 },
-        { red: 0.27450982, green: 0.7411765, blue: 0.7764706 },
-        { red: 0.40392157, green: 0.30588236, blue: 0.654902 },
-        { red: 1 },
-        { red: 1, blue: 1 },
-        { red: 1, green: 1 },
-        { red: 0.91764706, green: 0.2627451, blue: 0.20784314 },
-        { red: 0.91764706, green: 0.2627451, blue: 0.20784314 }
-      ]
       var last_k = 0;
-      for (i=1; i<9; i++) {
+      for (i=1; i<numLines; i++) {
         var curve = new Bezier(seq[1],
                                seq[0],
                                seq[2],
@@ -45,47 +38,38 @@ doc.useServiceAccountAuth(require('./bc-map-268007-7e2ca0c52eb6.json'))
         seq = lcg(mapSize*i,1, mapSize, 6)
         seq[0] = end[0];
         seq[1] = end[1];
-        var LUT = curve.getLUT(mapSize * 2);
+        var LUT = curve.getLUT(numLutPoints);
         LUT.forEach(function(p, k) { 
           var x = Math.floor(p.x);
           var y = Math.floor(p.y);
           var point = sheet.getCell(x,y);
-
+          var rgb = lcg(mapSize-i,0, 255, 3)
+          var colours = {red: rgb[0], green: rgb[1], blue: rgb[2]}
           // Vertical lines every N
-          if (k % 50 === 0) {
+          if (k % (mapSize / 2) === 0) {
             if (k != last_k) {
               var j = 0;
               for (j = 0; j < mapSize; j++) {
-                var sidepoint = sheet.getCell(j, y)              
-                sidepoint.backgroundColor = colours[i];
+                var sidepoint = sheet.getCell(j, y);
+                sidepoint.backgroundColor = colours;
               }
             }
             last_k = k;
           } 
-          if (k === 0) {
+          if (k === 0 || k === numLutPoints -1) {
             var j = 0;
             for (j = 0; j < mapSize; j++) {
-              var sidepoint = sheet.getCell(y, j)              
-              sidepoint.backgroundColor = colours[i];
+              var sidepoint = sheet.getCell(x, j)              
+              sidepoint.backgroundColor = colours;
             }
           }
 
-          point.backgroundColor = colours[i];
+          point.backgroundColor = colours;
         });
         sheet.saveUpdatedCells(); // save all updates in one call      
       }
 
     })
-
-    //   sheet.updateDimensionProperties("COLUMNS", 
-    //                                   {pixelSize: 5},
-    //                                   {startIndex: 0, endIndex: 99})
-    // })
-    // .then(function(value) {
-    //   const sheet = doc.sheetsByIndex[0];
-    //   sheet.updateDimensionProperties("ROWS", 
-    //                                   {pixelSize: 5},
-    //                                   {startIndex: 0, endIndex: 99})
 
     })},
         reason => {console.log(reason)});
